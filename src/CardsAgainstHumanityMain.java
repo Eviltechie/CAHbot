76c97ae26e913b718a9122e7829acfb9de4734cf
Ivan Panarusky <eviltechie@joe.to>
2012-07-07 00:30:09 -0400

    first commit

---
+++ src/CardsAgainstHumanityMain.java
@@ -0,0 +1,11 @@
+
+public class CardsAgainstHumanityMain {
+	public static void main(String[] args) throws Exception {
+		CardsAgainstHumanity bot = new CardsAgainstHumanity();
+		bot.setVerbose(true);
+		bot.connect("irc.gamesurge.net");
+		bot.joinChannel("#joe.to");
+		bot.setMessageDelay(2500);
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+
+public class Player {
+	private String name;
+	private ArrayList<String> whiteCards = new ArrayList<String>();
+	private int awesomePoints = 0;
+	private CardsAgainstHumanity ircBot;
+	public String playedCard;
+	
+	public Player(String name, CardsAgainstHumanity ircBot) {
+		this.name = name;
+		this.ircBot = ircBot;
+		drawTo10();
+		showCardsToPlayer();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void addPoint() {
+		awesomePoints++;
+	}
+	
+	public int getScore() {
+		return awesomePoints;
+	}
+	
+	public void drawTo10() {
+		while (whiteCards.size() < 10) {
+			whiteCards.add(ircBot.nextWhiteCard());
+		}
+	}
+	
+	public void showCardsToPlayer() {
+		String cards = "Your cards are ";
+		int cardNumber = 1;
+		for (String c : whiteCards) {
+			cards += cardNumber + ") \u00031,0[" + c + "]\u0003  ";
+			cardNumber++;
+		}
+		ircBot.sendNotice(getName(), cards);
+		//ircBot.sendMessage(getName(), cards);
+	}
+	
+	public void playCard(String cardString) {
+		if (playedCard != null) {
+			ircBot.sendMessage(ircBot.channel, getName() + ": You can't play twice");
+			return;
+		}
+		String[] cardStrings = cardString.split(" ");
+		ArrayList<String> cardsToRemove = new ArrayList<String>();
+		playedCard = "";
+		for (String s : cardStrings) {
+			System.out.println("Attempting to parse " + "\"" + s.replaceAll(" ", "") + "\"");
+			int cardNumber = Integer.parseInt(s);
+			cardNumber--;
+			try {
+				playedCard += "\u00031,0[" + whiteCards.get(cardNumber) + "]\u0003 ";
+				cardsToRemove.add(whiteCards.get(cardNumber));
+			} catch (IndexOutOfBoundsException e) {
+				cardNumber++;
+				ircBot.sendMessage(ircBot.channel, getName() + ": You do not appear to have a " + cardNumber + ircBot.getOrdinal(cardNumber) + " card to play");
+				playedCard = null;
+				return;
+			}
+		}
+		HashSet<String> playedCards = new HashSet<String>(cardsToRemove);
+		if (playedCards.size() != cardsToRemove.size()) {
+			ircBot.sendMessage(ircBot.channel, getName() + ": You can't play the same card twice");
+			return;
+		}
+		for (String card : cardsToRemove) {
+			whiteCards.remove(card);
+		}
+		ircBot.sendMessage(ircBot.channel, getName() + ": Card(s) received");
+		ircBot.checkForPlayedCards();
+	}
+}
+ 
